52cb5c9369eafed35c12b50030413d4678b92f93
Martin Vaňa <martinva32@gmail.com>
2026-06-12 15:10:35 +0200

    feat(core-common): countdownSeconds flow utility

---
+++ core/common/src/test/kotlin/cz/vanama/courtflow/core/common/time/CountdownTest.kt
@@ -0,0 +1,38 @@
+package cz.vanama.courtflow.core.common.time
+
+import app.cash.turbine.test
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import io.kotest.matchers.types.shouldBeInstanceOf
+import org.junit.Test
+
+class CountdownTest {
+    @Test
+    fun `counts down from the given number to zero, one tick per second`() =
+        runTest {
+            countdownSeconds(3).test {
+                awaitItem() shouldBe 3
+                awaitItem() shouldBe 2
+                awaitItem() shouldBe 1
+                awaitItem() shouldBe 0
+                awaitComplete()
+            }
+        }
+
+    @Test
+    fun `zero emits a single zero and completes`() =
+        runTest {
+            countdownSeconds(0).test {
+                awaitItem() shouldBe 0
+                awaitComplete()
+            }
+        }
+
+    @Test
+    fun `negative input is rejected`() =
+        runTest {
+            countdownSeconds(-1).test {
+                awaitError().shouldBeInstanceOf<IllegalArgumentException>()
+            }
+        }
+}
