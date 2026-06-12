1669aaa570553eec3a317a5a040014d768e02f97
Martin Vaňa <martinva32@gmail.com>
2026-06-12 15:41:39 +0200

    build(core-common): apply android-library convention plugin

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,16 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("courtflow.android.library")
 }
 
 android {
     namespace = "cz.vanama.courtflow.core.common"
-    compileSdk = 37
-
-    defaultConfig {
-        minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
 
     testOptions {
         unitTests {
