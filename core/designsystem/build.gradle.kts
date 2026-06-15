c1c9fd2da1ad6eb7fef41610279cdf1bd6e71692
Martin Vaňa <martinva32@gmail.com>
2026-06-15 13:51:13 +0200

    build(designsystem,players,app): add compose-animation dependency

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.animation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
